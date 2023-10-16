b0327443a9e5bfbe922d73f5fcb492ac686824ea
ragavanv7 <104961083+ragavanv7@users.noreply.github.com>
2023-10-16 18:31:38 +0530

    Add files via upload

---
+++ Day-13/src/main/java/com/missionpossible/ragavan/dto/request/ContactRequest.java
@@ -0,0 +1,26 @@
+package com.missionpossible.ragavan.dto.request;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContactRequest {
+	private Long sid;
+	private String service_name;
+	private String service_description;
+	private String service_type;
+	private String service_owner;
+	private String service_status;
+	private String service_cost;
+	private String service_duration;
+	private String service_availabe;
+}
+
+
+
---
+++ Day-13/src/main/java/com/missionpossible/ragavan/model/enumerate/Role.java
@@ -0,0 +1,9 @@
+package com.missionpossible.ragavan.model.enumerate;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum Role {
+    ADMIN,
+    CUSTOMER
+}
