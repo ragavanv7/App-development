98a16a64b9e27379d1b2a618fced0518ec334cd5
ragavanv7 <104961083+ragavanv7@users.noreply.github.com>
2023-10-16 17:59:21 +0530

    Delete Day-14 directory

--- Day-14/src/main/java/com/mission/naveen/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mission.naveen.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mission.naveen.Model.User;
-
-public interface UserRepository extends JpaRepository<User,String>{
-    
-}
