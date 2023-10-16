96db164da4aebd448066d6ff0165bed23cf62c2e
ragavanv7 <104961083+ragavanv7@users.noreply.github.com>
2023-10-16 18:34:34 +0530

    Add files via upload

---
+++ Day-14/src/main/java/com/missionpossible/ragavan/RagavanApplication.java
@@ -0,0 +1,14 @@
+package com.missionpossible.ragavan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagavanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagavanApplication.class, args);
+	}
+	
+	
+}
---
+++ Day-14/src/main/java/com/missionpossible/ragavan/repository/ApplyPolicyRepository.java
@@ -0,0 +1,10 @@
+package com.missionpossible.ragavan.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.missionpossible.ragavan.model.ApplyPolicy;
+
+
+public interface ApplyPolicyRepository extends JpaRepository<ApplyPolicy, Integer>{
+
+}
